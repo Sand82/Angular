809751f2fae858f0965f756c93b4937fc6c7f308
Sand82 <sandoki@abv.bg>
2023-01-23 23:07:41 +0200

    added new exercise

---
+++ angular-course-1-components/src/app/course-card/course-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
+  @Input()
+  cardIndex: number;
+
+  @Output("courseSelected")
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCourseViewed() {
+    console.log("card component - button clicked...");
+
+    this.courseEmitter.emit(this.course);
+  }
+}
