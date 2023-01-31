36e3134efa9f5e1f334c33468d09de2fa125180c
Sand82 <sandoki@abv.bg>
2023-01-31 17:01:41 +0200

    added current training functionality

--- fitness-tracker/src/app/training/stopTraining/stop-training-component.ts
+++ fitness-tracker/src/app/training/stopTraining/stop-training-component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-stop-training',
-  template: `<h1 mat-dialog-title>Are you sure?</h1>
+  template: `<div
+    fxFlex="300px"
+    fxLayout="column"
+    fxLayoutAlign="center center"
+  >
+    <h1 mat-dialog-title>Are you sure?</h1>
+    <div mat-dailog-content>
+      <p>You already got {{ data.progress }}%</p>
+    </div>
     <div mat-dialog-actions>
       <button mat-button [mat-dialog-close]="true">Yes</button>
       <button mat-button [mat-dialog-close]="false">No</button>
-    </div>`,
+    </div>
+  </div>`,
 })
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    console.log(data);
+  }
+}
