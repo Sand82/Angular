fcc577e5e0cc7ca79226f1c35d7a2e19803b4586
Sand82 <sandoki@abv.bg>
2024-11-15 00:07:07 +0200

    added some changes

--- form/src/app/form/form.component.ts
+++ form/src/app/form/form.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormGroup, FormBuilder],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form! : FormGroup;
 
+  constructor(private fb : FormBuilder){}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    })
+  }  
 }
