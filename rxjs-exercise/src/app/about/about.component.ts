45eb30b66e02e49420443d5de67a6db8da23d1d4
Sand82 <sandoki@abv.bg>
2024-11-18 21:27:15 +0200

    added new exercise

--- rxjs-exercise/src/app/about/about.component.ts
+++ rxjs-exercise/src/app/about/about.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  // RxJs OBSERVABLE
+  // ngOnInit() {
+  //   const interval$ = interval(1000);
+  //   interval$.subscribe((val) => console.log("stream 1 " + val));
+  //   interval$.subscribe((val) => console.log("stream 2 " + val));
 
+  //   const timer$ = timer(3000, 1000);
+  //   const sub = timer$.subscribe((val) => console.log("for timer " + val));
+
+  //   setTimeout(() => {
+  //     sub.unsubscribe();
+  //   }, 5000);
+
+  //   const click$ = fromEvent(document, "click");
+
+  //   click$.subscribe(
+  //     (eve) => console.log(eve),
+  //     (err) => console.log(err),
+  //     () => console.log("complete")
+  //   );
+  // }
+
+  // STREAMS OF VALUES
+
+  // ngOnInit() {
+  //
+
+  //   document.addEventListener("click", (event) => {
+  //     console.log(event);
+  //   });
+
+  //   let count = 0;
+
+  //   setInterval(() => {
+  //     console.log(count);
+  //     count++;
+  //   }, 1000);
+
+  //   setTimeout(() => {
+  //     console.log("finished...");
+  //   }, 3000);
+
+  //   //CALLBACK HELL
+  //   document.addEventListener("click", (event) => {
+  //     console.log(event);
+
+  //     setTimeout(() => {
+  //       console.log("finished...");
+
+  //       setInterval(() => {
+  //         console.log(count);
+  //         count++;
+  //       }, 1000);
+  //     }, 3000);
+  //   });
+  // }
 }
