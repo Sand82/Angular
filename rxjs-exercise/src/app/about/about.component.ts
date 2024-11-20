fb30108f2726ef8a9f85974f049c2a6f3d5fb1ce
Sand82 <sandoki@abv.bg>
2024-11-20 20:44:31 +0200

    added new exercise

--- rxjs-exercise/src/app/about/about.component.ts
+++ rxjs-exercise/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { fromEvent, interval, timer } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { createHttpObservable } from "../common/util";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "about",
@@ -9,62 +10,97 @@ import { fromEvent, interval, timer } from "rxjs";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    const http$ = createHttpObservable("api/courses");
 
-  // RxJs OBSERVABLE
-  // ngOnInit() {
-  //   const interval$ = interval(1000);
-  //   interval$.subscribe((val) => console.log("stream 1 " + val));
-  //   interval$.subscribe((val) => console.log("stream 2 " + val));
+    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
 
-  //   const timer$ = timer(3000, 1000);
-  //   const sub = timer$.subscribe((val) => console.log("for timer " + val));
-
-  //   setTimeout(() => {
-  //     sub.unsubscribe();
-  //   }, 5000);
-
-  //   const click$ = fromEvent(document, "click");
-
-  //   click$.subscribe(
-  //     (eve) => console.log(eve),
-  //     (err) => console.log(err),
-  //     () => console.log("complete")
-  //   );
-  // }
-
-  // STREAMS OF VALUES
-
-  // ngOnInit() {
-  //
-
-  //   document.addEventListener("click", (event) => {
-  //     console.log(event);
-  //   });
-
-  //   let count = 0;
-
-  //   setInterval(() => {
-  //     console.log(count);
-  //     count++;
-  //   }, 1000);
-
-  //   setTimeout(() => {
-  //     console.log("finished...");
-  //   }, 3000);
-
-  //   //CALLBACK HELL
-  //   document.addEventListener("click", (event) => {
-  //     console.log(event);
-
-  //     setTimeout(() => {
-  //       console.log("finished...");
-
-  //       setInterval(() => {
-  //         console.log(count);
-  //         count++;
-  //       }, 1000);
-  //     }, 3000);
-  //   });
-  // }
+    courses$.subscribe((res) => console.log(res));
+  }
 }
+
+///////////////////
+//CUSTOM OBSERVABLE
+///////////////////
+
+// ngOnInit() {
+//   const observable$ = new Observable((observer) => {
+//     fetch("api/courses")
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((body) => {
+//         observer.next(body);
+//         observer.complete();
+//       })
+//       .catch((err) => {
+//         observer.error(err);
+//       });
+//   });
+
+//   observable$.subscribe((cources) => {
+//     console.log(cources);
+//   });
+// }
+
+///////////////////
+//RxJs OBSERVABLE
+///////////////////
+
+// ngOnInit() {
+//   const interval$ = interval(1000);
+//   interval$.subscribe((val) => console.log("stream 1 " + val));
+//   interval$.subscribe((val) => console.log("stream 2 " + val));
+
+//   const timer$ = timer(3000, 1000);
+//   const sub = timer$.subscribe((val) => console.log("for timer " + val));
+
+//   setTimeout(() => {
+//     sub.unsubscribe();
+//   }, 5000);
+
+//   const click$ = fromEvent(document, "click");
+
+//   click$.subscribe(
+//     (eve) => console.log(eve),
+//     (err) => console.log(err),
+//     () => console.log("complete")
+//   );
+// }
+
+///////////////////
+//STREAMS OF VALUES
+///////////////////
+
+// ngOnInit() {
+//
+
+//   document.addEventListener("click", (event) => {
+//     console.log(event);
+//   });
+
+//   let count = 0;
+
+//   setInterval(() => {
+//     console.log(count);
+//     count++;
+//   }, 1000);
+
+//   setTimeout(() => {
+//     console.log("finished...");
+//   }, 3000);
+
+//   //CALLBACK HELL
+//   document.addEventListener("click", (event) => {
+//     console.log(event);
+
+//     setTimeout(() => {
+//       console.log("finished...");
+
+//       setInterval(() => {
+//         console.log(count);
+//         count++;
+//       }, 1000);
+//     }, 3000);
+//   });
+// }
