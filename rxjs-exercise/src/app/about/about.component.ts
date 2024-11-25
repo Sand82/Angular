c11deebf529ff4afe9050bfdcb6a128320088e7d
Sand82 <sandoki@abv.bg>
2024-11-25 18:36:31 +0200

    added new exercise

--- rxjs-exercise/src/app/about/about.component.ts
+++ rxjs-exercise/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { concat, of } from "rxjs";
+import { concat, of, interval, merge } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "about",
@@ -11,6 +12,21 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {}
 }
+
+////////////////////////////
+//MERGE OBSERVABLES
+///////////////////////////
+
+// ngOnInit(): void {
+
+//   const interval1$ = interval(1000);
+
+//   const interval2$ = interval1$.pipe(map(val => 10 * val));
+
+//   const result$ = merge(interval1$, interval2$);
+
+//   result$.subscribe(res => console.log(res));
+// }
 
 ////////////////////////////
 //OPSERVABLES CONCATENATION
